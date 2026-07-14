7631c8d880713b8b08480ffed6642fd5a8c8d1bb
Howard Rincon <howard.rincon@pursuit.org>
2026-07-14 10:51:04 -0400

    Add event listeners for conversion buttons

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Road Trip Converter Loaded!");
+const toKm = document.getElementById("toKm");
+const toMiles = document.getElementById("toMiles");
+
+toKm.addEventListener("click", () => {
+
+    console.log("Miles ➜ Kilometers");
+
+});
+
+toMiles.addEventListener("click", () => {
+
+    console.log("Kilometers ➜ Miles");
+
+});
